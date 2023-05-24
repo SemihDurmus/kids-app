47fcc1e126e4a82fdfd63f37e9bcef05aca90189
Semih Durmus <semihdurmus@yahoo.com>
2023-05-24 07:26:52 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Points from "pages/points/Points";
+import Welcome from "pages/welcome/Welcome";
+import NavBar from "maincomponents/navbar/Navbar";
+import MultiplicationGame from "pages/multiplicationGame/MultiplicationGame";
 
 function App() {
   return (
-    <div>
-      <h1>Kids app</h1>
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/multiplication-game" element={<MultiplicationGame />} />
+        <Route path="/points" element={<Points />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/maincomponents/navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const NavBar = (): ReactElement => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <button onClick={() => navigate("/")}>Home</button>
+      <button onClick={() => navigate("/multiplication-game")}>
+        Multiplication
+      </button>
+      <button onClick={() => navigate("/points")}>Points</button>
+    </div>
+  );
+};
+
+export default NavBar;
