93afa1c40854ba4b3aef88ec6c1eff9c85d92240
Semih Durmus <semihdurmus@yahoo.com>
2023-05-25 23:20:41 +0200

    Style Choice

--- src/pages/multiplication/Choice.tsx
+++ src/pages/multiplication/Choice.tsx
@@ -1,13 +1,50 @@
+import { Box } from "@mui/material";
 import { ReactElement } from "react";
-import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 export const Choice = (): ReactElement => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <Link to="exercise">Exercise </Link>
-      <Link to="game">Game</Link>
-    </div>
+    <Wrapper>
+      <Box sx={{ display: "flex" }}>
+        <BigButton onClick={() => navigate("exercise")}>Exercise</BigButton>
+        <BigButton onClick={() => navigate("game")}>Game</BigButton>
+      </Box>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 92vh;
+  background-color: #ecf0f1;
+`;
+
+const BigButton = styled.div`
+  height: 5rem;
+  width: 5rem;
+  line-height: 5rem;
+  text-align: center;
+  border: 1px solid #34495e;
+  border-radius: 1rem;
+  box-shadow: 0px 10px 22px -3px rgba(0, 0, 0, 0.1);
+  font-family: "Short stack", cursive;
+  font-weight: bold;
+  font-size: 1rem;
+  color: #6f1e51;
+  &:first-of-type {
+    margin-right: 1rem;
+    color: #16a085;
+  }
+  &:hover {
+    cursor: pointer;
+    background-color: #34495e;
+    color: #fff;
+  }
+`;
 
 export default Choice;
